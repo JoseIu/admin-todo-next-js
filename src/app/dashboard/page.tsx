850bf9d79efb7ac4f201f9b17033fc2f617dafd1
Joselu <joseluissgz97@gmail.com>
2024-09-26 21:29:56 +0200

    add: DashboardPage

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components';
+
+const DashboardPage = () => {
+  return (
+    <section>
+      <Header />
+
+      <span>DashboardPage</span>
+    </section>
+  );
+};
+
+export default DashboardPage;
