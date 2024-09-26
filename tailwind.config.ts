51fa24cf4169bb9ff82a8a10cd735cc045b0ecfb
Joselu <joseluissgz97@gmail.com>
2024-09-26 21:29:10 +0200

    add: colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blueDark: '#1C1D22',
+        blueSemiDark: '#222327',
+        blueLight: '#2A2B2F',
+        todoGg: '#24262C',
+        textWhite: '#FFFFFF',
+        textGray: '#7F7F7F',
+        backgroundFocus: '#24262C',
+        todoDone: '#FF7979',
       },
     },
   },
