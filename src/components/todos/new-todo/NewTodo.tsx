f706cd04f2e3db663c054c5ccca4715c9745f7fa
Joselu <joseluissgz97@gmail.com>
2024-09-27 10:42:45 +0200

    add: FRONT handle add new todo and deltetodoscompleted

--- src/components/todos/new-todo/NewTodo.tsx
+++ src/components/todos/new-todo/NewTodo.tsx
@@ -1,21 +1,33 @@
 'use client';
 
+import { deleteTodosCompletedFetch, todoFetchCreate } from '@/helpers/todoFetch';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export const NewTodo = () => {
   const [showForm, setShowFrom] = useState(false);
   const [todo, setTodo] = useState('');
+  const router = useRouter();
   const isActiveForm = showForm ? 'opacity-100' : 'opacity-0 pointer-events-none';
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (!todo) return;
-    console.log(todo);
+    if (!todo.trim()) return;
+    await todoFetchCreate(todo.trim());
+    setTodo('');
+    router.refresh();
+  };
+  const handleDeleteTodosCompleted = async () => {
+    await deleteTodosCompletedFetch();
+    router.refresh();
   };
 
   return (
     <div className="w-full p-4 flex items-center justify-between">
-      <form onSubmit={handleSubmit} className={`flex items-center gap-3 ${isActiveForm} `}>
+      <form
+        onSubmit={handleSubmit}
+        className={`flex items-center gap-3 ${isActiveForm} transition-opacity duration-150 `}
+      >
         <input
           className="bg-blueDark py-2 px-4 rounded-md"
           type="text"
@@ -29,9 +41,12 @@ export const NewTodo = () => {
         </button>
       </form>
 
-      <button className="py-2 px-4 bg-btnColor rounded-3xl" onClick={() => setShowFrom(!showForm)}>
-        {showForm ? 'Cancel' : 'New todo'}
-      </button>
+      <div>
+        <button className="py-2 px-4 bg-btnColor rounded-3xl" onClick={() => setShowFrom(!showForm)}>
+          {showForm ? 'Cancel' : 'New todo'}
+        </button>
+        <button onClick={handleDeleteTodosCompleted}>Delete completed</button>
+      </div>
     </div>
   );
 };
--- src/helpers/todoFetch.ts
+++ src/helpers/todoFetch.ts
@@ -13,7 +13,33 @@ export const todoFetch = async (id: string, completed: boolean): Promise<TodoI> 
 
   const todo = await todoUpdated.json();
 
-  console.log(todo);
+  return todo;
+};
+export const todoFetchCreate = async (description: string): Promise<TodoI> => {
+  const body = { description };
+
+  const todoUpdated = await fetch(`/api/todos`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const todo = await todoUpdated.json();
+
+  return todo;
+};
+
+export const deleteTodosCompletedFetch = async (): Promise<TodoI> => {
+  const todoUpdated = await fetch(`/api/todos`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const todo = await todoUpdated.json();
 
   return todo;
 };
