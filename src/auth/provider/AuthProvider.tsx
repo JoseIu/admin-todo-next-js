6102ebcf5329e8874c558c704fef17ef273dae4a
Joselu <joseluissgz97@gmail.com>
2024-09-30 18:30:30 +0200

    add: Auth.js config

---
+++ src/auth/provider/AuthProvider.tsx
@@ -0,0 +1,9 @@
+import { SessionProvider } from 'next-auth/react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const AuthProvider = ({ children }: Props) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
